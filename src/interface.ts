23e0ece4026e634118892152b2754efb380bce7b
Arief Rohman <didadadida93@gmail.com>
2020-03-21 18:44:57 +0700

    add interface

---
+++ src/interface.ts
@@ -0,0 +1,18 @@
+export interface SessionInterface {
+  session: string;
+  cookie: string;
+  age: Date;
+}
+
+export interface AccountInterface {
+  email: string;
+  password: string;
+  worldname: string;
+}
+
+export interface StoreInterface {
+  msid: string;
+  account: AccountInterface;
+  lobby: SessionInterface;
+  gameworld: SessionInterface;
+}
