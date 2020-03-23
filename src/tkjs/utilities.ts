c4bbde6a1f14b0b87b4e8f8d1ca16a51d1bcc775
Arief Rohman <didadadida93@gmail.com>
2020-03-23 20:51:22 +0700

    add another utilities

--- src/tkjs/utilities.ts
+++ src/tkjs/utilities.ts
@@ -1,3 +1,5 @@
+import { ExtractorNotFoundError, UndefinedTypeError } from "./errors";
+
 export function range(_p: number, _t?: number, _s?: number): Array<number> {
   /**
    * @param <_p> Return a list integers of zero until <_p> value.
@@ -39,4 +41,49 @@ export function convertToCoordinate(cellId: string): Array<number> {
   const realy: number = parseInt(ycord, 2) - 16384;
 
   return [realx, realy];
+}
+
+export function extractMsid(source: string): string {
+  const msid: string | null = /msid=([\w]*)&msname/g.exec(source);
+  if (!msid) throw new ExtractorNotFoundError("MSID not found");
+  return msid[1];
+}
+
+export function extractToken(source: string): string {
+  const token: string | null = /token=([\w]*)&msid/g.exec(source);
+  if (!token) throw new ExtractorNotFoundError("Token not found");
+  return token[1];
+}
+
+export function extractSession(source: string): string {
+  const session: string | null = /%7B%22key%22%3A%22([\w]*)%22%2C%22/g.exec(source);
+  if (!session) throw new ExtractorNotFoundError("Session not found");
+  return session[1];
+}
+
+export function extractSessionAge(source: string): Date {
+  return new Date(cookieString.split(";")[1].split("=")[1]);
+}
+
+export function extractor({ type, value }: { type: string; value: string }): string | Date {
+  switch (type) {
+    case "msid":
+      return extractMsid(value);
+      break;
+
+    case "token":
+      return extractToken(value);
+      break;
+
+    case "session":
+      return extractSession(value);
+      break;
+
+    case "session age":
+      return extractSessionAge(value);
+      break;
+
+    default:
+      throw new UndefinedTypeError(`Undefined extractor type: ${type}`);
+  }
 }
