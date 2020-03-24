52d34ad8436d4dc094993daed4600a675ff776c6
Arief Rohman <didadadida93@gmail.com>
2020-03-24 11:22:41 +0700

    add additional class method on Gameworld object

--- src/tkjs/driver/gameworld.ts
+++ src/tkjs/driver/gameworld.ts
@@ -5,8 +5,9 @@ import URL from "./url";
 import store from "../store";
 import { userAgent } from "../constants";
 import { extractor } from "../utilities";
-import { SessionInterface } from "../../interface";
+import { SessionNotFoundError } from '../errors'
 import { updateState, resetState, resetWorldname } from "../store";
+import { SessionInterface, RequestPayloadInterface, StoreInterface } from "../../interface";
 
 class Gameworld {
   static driver: AxiosInstance = axios.create({ headers: { ...userAgent } });
@@ -45,6 +46,43 @@ class Gameworld {
 
       throw err;
     }
+  }
+
+  static async hitServer({ action, controller, params }: RequestPayloadInterface): Promise<any> {
+    const { msid, lobby, gameworld }: StoreInterface = store.getState();
+    const { cookie: lobbyCookie }: SessionInterface = lobby;
+    const { cookie: gameworldCookie, session: gameworldSession }: SessionInterface = gameworld;
+
+    if (!msid || !lobbyCookie || !gameworldCookie || !gameworldSession) {
+      throw new SessionNotFoundError();
+    }
+
+    const cookie: string = lobbyCookie + msid + "; " + gameworldCookie;
+    const query: string = `/?c=${controller}&a=${action}&t${Date.now()}`;
+
+    const response: AxiosResponse = await Gameworld.driver.post(
+      URL.GAMEWORLD_API + query,
+      { action, controller, params, session: gameworldSession },
+      { headers: { cookie } }
+    );
+
+    return response.data;
+  }
+
+  static async getCache(params: any): Promise<any> {
+    const payload: RequestPayloadInterface = {
+      action: "get",
+      controller: "cache",
+      params,
+    };
+
+    const data: any = await Gameworld.hitServer(payload);
+    return data;
+  }
+
+  static async getOwnVillageList(): Promise<Array<any>> {
+    const data: any = await Gameworld.getCache({ names: ["Collection:Village:own"] });
+    return data.cache[0].data.cache.map((village: any) => village.data); // only need village data
   }
 }
 
