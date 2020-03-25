df26c50285d00a087fcd59af3bde5324040e8b6b
Arief Rohman <didadadida93@gmail.com>
2020-03-25 14:52:23 +0700

    Update utilities
    * fix bugs on range function when start, stop, or end is filled with 0
    * function convertToCoordinate now return CoordinateInterface type
    * function distance now more described

--- src/tkjs/utilities.ts
+++ src/tkjs/utilities.ts
@@ -1,3 +1,4 @@
+import { CoordinateInterface } from "../interface";
 import { ExtractorNotFoundError, UndefinedTypeError } from "./errors";
 
 export function range(_p: number, _t?: number, _s?: number): Array<number> {
@@ -8,9 +9,9 @@ export function range(_p: number, _t?: number, _s?: number): Array<number> {
    * @return Return a array list
    */
 
-  let start: number = _t ? _p : 0;
-  let stop: number = _t ? _t : _p;
-  let step: number = _s ? _s : 1;
+  let start: number = _t !== undefined ? _p : 0;
+  let stop: number = _t !== undefined ? _t : _p;
+  let step: number = _s !== undefined ? _s : 1;
 
   let t: Array<number> = [];
   for (let i = start; i < stop; i = i + step) {
@@ -29,7 +30,7 @@ export function convertToCellId(x: number, y: number): string {
   return String(rx + ry);
 }
 
-export function convertToCoordinate(cellId: string): Array<number> {
+export function convertToCoordinate(cellId: string): CoordinateInterface {
   let binary: string = parseInt(cellId, 10).toString(2);
 
   if (binary.length < 30) binary = "0" + binary;
@@ -40,7 +41,7 @@ export function convertToCoordinate(cellId: string): Array<number> {
   const realx: number = parseInt(xcord, 2) - 16384;
   const realy: number = parseInt(ycord, 2) - 16384;
 
-  return [realx, realy];
+  return { x: realx, y: realy };
 }
 
 export function extractMsid(source: string): string {
@@ -86,4 +87,8 @@ export function extractor({ type, value }: { type: string; value: any }): any {
     default:
       throw new UndefinedTypeError(`Undefined extractor type: ${type}`);
   }
+}
+
+export function distance(source: CoordinateInterface, target: CoordinateInterface): number {
+  return Math.sqrt((source.x - target.x) ** 2 + (source.y - target.y) ** 2);
 }
